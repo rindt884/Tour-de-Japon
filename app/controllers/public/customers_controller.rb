856d162d9030d1dcf2fc5f70710a88047635c7d9
rindt884 <tatsuya.121127@gmail.com>
2022-09-03 10:23:44 +0900

    コード修正

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -46,7 +46,7 @@ class Public::CustomersController < ApplicationController
     
     def withdrawal
       @customer = Customer.find(params[:id])
-      @customer.update(is_deleted: true) # is_deletedカラムをtrueに変更することにより削除フラグを立てる
+      @customer.update(is_deleted: true) # is_deletedカラムをtrueに変更することにより退会フラグを立てる
       reset_session
       flash[:notice] = "退会処理を実行いたしました"
       redirect_to new_customer_registration_path
